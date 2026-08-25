dec97e172f77c3148065fcb5fc0a72b0a74b6b1d
misoa <misoarakotoniaina@gmail.com>
2026-08-25 19:55:36 +0300

    feat(service): get examen by their id

--- src/Service/ExamenService.ts
+++ src/Service/ExamenService.ts
@@ -1,8 +1,12 @@
-import { ExamenPublique } from '../model/Examen';
+import { Examen, ExamenPublique } from '../model/Examen';
 
 export class ExamenService {
     async getAllExams(): Promise<ExamenPublique[]> {
         const examens: ExamenPublique[] = [];
     return examens;
   }
+
+async getExamById(id: number): Promise<Examen | null> {
+    return null;
+}
 }
