629a0062b289914461393b79d3a7abf25dd67e54
misoa <misoarakotoniaina@gmail.com>
2026-08-25 19:53:24 +0300

    feat(service): recuperate all examen public

---
+++ src/Service/ExamenService.ts
@@ -0,0 +1,8 @@
+import { ExamenPublique } from '../model/Examen';
+
+export class ExamenService {
+    async getAllExams(): Promise<ExamenPublique[]> {
+        const examens: ExamenPublique[] = [];
+    return examens;
+  }
+}
