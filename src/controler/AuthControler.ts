de53f0caf47340c17bb42b5579373db32a652195
CHAN KWONG Stanley <falconcocs3@gmail.com>
2026-08-26 12:17:23 +0300

    fix : mise en format class AuthMiddleWare.ts

--- src/controler/AuthControler.ts
+++ src/controler/AuthControler.ts
@@ -1,19 +1,24 @@
 import { Request, Response } from 'express';
 import { Login } from '../model/Compte';
-import * as authService from '../Service/AuthService';
+import { AuthService } from '../Service/AuthService';
 
-export async function login(req: Request, res: Response): Promise<void> {
-  const credentials: Login = req.body;
+export class AuthControler {
+  constructor(private authService: AuthService) {}
 
-  if (!credentials.email || !credentials.password) {
-    res.status(400).json({ message: 'Email et mot de passe requis.' });
-    return;
-  }
+  login = async (req: Request, res: Response): Promise<void> => {
+    const credentials: Login = req.body;
 
-  try {
-    const result = await authService.login(credentials);
-    res.status(200).json(result);
-  } catch (err: any) {
-    res.status(401).json({ message: err.message });
-  }
+    if (!credentials.email || !credentials.password) {
+      res.status(400).json({ message: 'Email et mot de passe requis.' });
+      return;
+    }
+
+    try {
+      const result = await this.authService.login(credentials);
+      res.status(200).json(result);
+    } catch (err) {
+      const error = err as Error;
+      res.status(401).json({ message: error.message });
+    }
+  };
 }
